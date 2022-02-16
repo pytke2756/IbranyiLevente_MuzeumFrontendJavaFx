b5e517a5351f7fe6d49792d001eacffefdeae08b
Levente Ibrányi <ibranyilevente@gmail.com>
2022-02-16 21:16:37 +0100

    Sql hozzáadva

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.muzeumfrontendjavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.muzeumfrontendjavafx to javafx.fxml;

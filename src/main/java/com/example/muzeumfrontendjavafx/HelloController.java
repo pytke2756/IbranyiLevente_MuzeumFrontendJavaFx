00c6e8977ad2951cf46a01b5ce7326448245ddb0
Levente Ibrányi <ibranyilevente@gmail.com>
2022-02-17 11:54:16 +0100

    Main layout kész

--- src/main/java/com/example/muzeumfrontendjavafx/HelloController.java
+++ src/main/java/com/example/muzeumfrontendjavafx/HelloController.java
@@ -1,14 +1,50 @@
 package com.example.muzeumfrontendjavafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TableColumn colFestmenyCim;
+    @FXML
+    private TableColumn colFestmenyKiallitva;
+    @FXML
+    private TableColumn colFestmenyEv;
+    @FXML
+    private TableView szobrokTableView;
+    @FXML
+    private TableColumn colSzoborMagassag;
+    @FXML
+    private TableColumn colSzoborSzemely;
+    @FXML
+    private TableView festmenyekTableView;
+    @FXML
+    private TableColumn colSzoborAr;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onClickFestmenyModositasButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onClickFestmenyHozzaadasButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onClickFestmenyTorlesButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onClickSzoborModositasButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onClickSzoborTorlesButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onClickSzoborHozzaadasButton(ActionEvent actionEvent) {
     }
 }
--- src/main/java/com/example/muzeumfrontendjavafx/MuzeumFrontendApp.java
+++ src/main/java/com/example/muzeumfrontendjavafx/MuzeumFrontendApp.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MuzeumFrontendApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MuzeumFrontendApp.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 700);
+        stage.setTitle("Muzeum Frontend");
         stage.setScene(scene);
         stage.show();
     }

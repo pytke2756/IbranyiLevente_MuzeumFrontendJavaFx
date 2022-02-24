8c0567d916b7546a6235bf20ccfb7db0844fbc03
Levente Ibrányi <ibranyilevente@gmail.com>
2022-02-24 14:17:30 +0100

    Gson hozzáadva

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,9 @@ module com.example.muzeumfrontendjavafx {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires com.google.gson;
 
 
-    opens com.example.muzeumfrontendjavafx to javafx.fxml;
+    opens com.example.muzeumfrontendjavafx to javafx.fxml, com.google.gson;
     exports com.example.muzeumfrontendjavafx;
 }
